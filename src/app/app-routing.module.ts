e65b928b377da36024c0be755198d854d91e7c50
Matias Fuentes <matiasfuentes2909@gmail.com>
2023-10-31 17:25:24 -0300

    Ruta navbar-menu realizada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NavbarMenuComponent } from './shared/components/navbar-menu/navbar-menu.component';
 
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path:'navbar-menu',
+    component:NavbarMenuComponent
   },
   {
     path: '',
